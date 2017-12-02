4b869670516e3551836d0d454309cf240ae7c77e
Charles Samborski <demurgos@demurgos.net>
2017-12-02 21:03:24 +0100

    Release v0.2.0
    - **[Breaking change]** Move the `detect` function to the
      `DesktopEnvironment` namespace
    - **[Fix]** Detect `Cinammon`

--- src/lib.rs
+++ src/lib.rs
@@ -16,32 +16,35 @@ pub enum DesktopEnvironment {
   Xfce,
 }
 
-#[cfg(target_os = "macos")]
-pub fn detect() -> DesktopEnvironment {
-  DesktopEnvironment::MacOs
-}
+impl DesktopEnvironment {
+  #[cfg(target_os = "macos")]
+  pub fn detect() -> Self {
+    DesktopEnvironment::MacOs
+  }
 
-#[cfg(target_os = "windows")]
-pub fn detect() -> DesktopEnvironment {
-  DesktopEnvironment::Windows
-}
+  #[cfg(target_os = "windows")]
+  pub fn detect() -> Self {
+    DesktopEnvironment::Windows
+  }
 
-#[cfg(not(any(target_os = "macos", target_os = "windows")))]
-pub fn detect() -> DesktopEnvironment {
-  match env::var("XDG_CURRENT_DESKTOP") {
-    Err(_) => DesktopEnvironment::Unknown,
-    Ok(current_desktop) => {
-      match current_desktop.as_str() {
-        "Cinnamon" => DesktopEnvironment::Cinnamon,
-        "ENLIGHTENMENT" => DesktopEnvironment::Enlightenment,
-        "GNOME" => DesktopEnvironment::Gnome,
-        "KDE" => DesktopEnvironment::Kde,
-        "LXDE" => DesktopEnvironment::Lxde,
-        "LXQt" => DesktopEnvironment::Lxqt,
-        "MATE" => DesktopEnvironment::Mate,
-        "Unity" => DesktopEnvironment::Unity,
-        "XFCE" => DesktopEnvironment::Xfce,
-        _ => DesktopEnvironment::Unknown,
+  #[cfg(not(any(target_os = "macos", target_os = "windows")))]
+  pub fn detect() -> Self {
+    match env::var("XDG_CURRENT_DESKTOP") {
+      Err(_) => DesktopEnvironment::Unknown,
+      Ok(current_desktop) => {
+        match current_desktop.as_str() {
+          "Cinnamon" => DesktopEnvironment::Cinnamon,
+          "ENLIGHTENMENT" => DesktopEnvironment::Enlightenment,
+          "GNOME" => DesktopEnvironment::Gnome,
+          "KDE" => DesktopEnvironment::Kde,
+          "LXDE" => DesktopEnvironment::Lxde,
+          "LXQt" => DesktopEnvironment::Lxqt,
+          "MATE" => DesktopEnvironment::Mate,
+          "Unity" => DesktopEnvironment::Unity,
+          "X-Cinnamon" => DesktopEnvironment::Cinnamon,
+          "XFCE" => DesktopEnvironment::Xfce,
+          _ => DesktopEnvironment::Unknown,
+        }
       }
     }
   }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate detect_desktop_environment;
 
-use detect_desktop_environment::{DesktopEnvironment, detect};
+use detect_desktop_environment::DesktopEnvironment;
 
 fn main() {
-  let de: DesktopEnvironment = detect();
+  let de: DesktopEnvironment = DesktopEnvironment::detect();
   println!("{:?}", de);
 }
